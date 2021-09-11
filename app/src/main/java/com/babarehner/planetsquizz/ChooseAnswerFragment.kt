9cb1815b0dee3231293df9e6e903e498eee52b51
mike <babarehner@gmail.com>
2021-09-11 00:45:21 -0400

    Got ChooseAnswerFragment to show up. Did not get the Question Number to pass through. Need to work on that next.

--- app/src/main/java/com/babarehner/planetsquizz/ChooseAnswerFragment.kt
+++ app/src/main/java/com/babarehner/planetsquizz/ChooseAnswerFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +18,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ChooseAnswerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var question: Int = 1
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +30,21 @@ class ChooseAnswerFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_choose_answer, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChooseAnswerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChooseAnswerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        val questionID = arguments?.getInt(QUESTION_ID, 1) ?: 1
+        setQuestionData(questionID)
     }
+
+
+    fun setQuestionData(questionID: Int) { question = questionID }
+
+    companion object {
+        private const val QUESTION_ID = "QUESTION_ID"
+        fun newInstance(questionID: Int) = ChooseAnswerFragment().apply {
+            arguments = Bundle().apply {
+                putInt(QUESTION_ID, questionID)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/babarehner/planetsquizz/MainActivity.kt
+++ app/src/main/java/com/babarehner/planetsquizz/MainActivity.kt
@@ -2,18 +2,40 @@ package com.babarehner.planetsquizz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import com.babarehner.planetsquizz.AnswerFragment.Companion.newInstance
+// import com.babarehner.planetsquizz.ChooseAnswerFragment.Companion.newInstance
 
-const val WHICH_QUESTION_ID = "WHICH_QUESTION_ID"
+const val QUESTION_ID = "QUESTION_ID"
 
-interface whichQuestionListener{ fun onSelected(id: Int)}
+interface QuestionListener{
+    fun onSelected(id: Int)
+}
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), QuestionListener{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // if first time around
+        if (savedInstanceState == null) {
+            // add the QuestionFragment to the fragment_container
+            findViewById<FragmentContainerView>(R.id.fragment_container)?.let{
+                frameLayout -> val questionFragment = QuestionFragment()
+                supportFragmentManager.beginTransaction().add(frameLayout.id, questionFragment)
+                    .commit()
+            }
+        }
     }
 
-    override fun onSelected(id: Int){
-        val questionFragment = supportFragmentManager.findFragmentById((R.id.))
+    override fun onSelected(questionID: Int){
+        findViewById<FragmentContainerView>(R.id.fragment_container)?.let{ frameLayout ->
+            val chooseAnswerFragment = ChooseAnswerFragment.newInstance(questionID)
+            //replace one fragment with another
+            supportFragmentManager.beginTransaction().replace(frameLayout.id, chooseAnswerFragment)
+                // addToBackStack null stops app from exiting when back button pressed
+                .addToBackStack(null).commit()
+            // app will now go back to ListFragment when back button pressed
+        }
     }
 }
